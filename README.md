8e421661faf7b063c479019db5418d108814a08c
DamianCwykiel <damian.cwykiel@gmail.com>
2019-01-28 22:20:28 +0100

    files updated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# #ViewConstructor#
-https://damiancwykiel.github.io/ViewConstructor/
+#ViewConstructor#
+* A project is one of exercises realized during WebDeveloper course. 
+* It has to show more or less a light copy of a program close to 'jsbin.com'.
+* This program allows to write and run a code that you want to see in a next pannel.
+* It's a nice place to check HTML or CSS features.
+
+* available under - https://damiancwykiel.github.io/ViewConstructor/
--- script.js
+++ script.js
@@ -1,48 +1,26 @@
-        function updateOutput() {
-
-            $("iframe").contents().find("html").html("<html><head><style>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
-
-            document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
-        }
-
-        $(".toggleButton").hover(function() {
-            $(this).addClass("highlighButton");
-
-
-        }, function() {
-            $(this).removeClass("highlighButton");
-
-
-        });
-
-        $(".toggleButton").click(function() {
-
-            $(this).toggleClass("active");
-
-            $(this).removeClass("highlighButton");
-
-            let panelId = $(this).attr("id") + "Panel";
-
-            $("#" + panelId).toggleClass("hidden");
-
-            let numberOfactivePanels = 4 - $(".hidden").length;
-
-
-
-            $(".panel").width(($(window).width() / numberOfactivePanels) - 10);
-
-        })
-
-        $(".panel").height($(window).height() - $(".header").height() - 20);
-
-        $(".panel").width(($(window).width() / 2) - 10);
-
-        updateOutput();
-
-
-        $("textarea").on("change keyup paste", function() {
-
-            updateOutput();
-
-
-        });
+/*VIEW_CONSTRUCTOR*/    
+        
+function updateOutput() {
+    $("iframe").contents().find("html").html("<html><head><style>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+}
+$(".toggleButton").hover(function() {
+     $(this).addClass("highlighButton");
+}, 
+function() {
+    $(this).removeClass("highlighButton");
+});
+$(".toggleButton").click(function() {
+    $(this).toggleClass("active");
+    $(this).removeClass("highlighButton");
+    let panelId = $(this).attr("id") + "Panel";
+    $("#" + panelId).toggleClass("hidden");
+    let numberOfactivePanels = 4 - $(".hidden").length;
+    $(".panel").width(($(window).width() / numberOfactivePanels) - 10);
+});
+$(".panel").height($(window).height() - $(".header").height() - 20);
+$(".panel").width(($(window).width() / 2) - 10);
+    updateOutput();
+$("textarea").on("change keyup paste", function() {
+    updateOutput();
+});
